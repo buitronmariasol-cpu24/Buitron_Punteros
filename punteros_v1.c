71a8d6657be04f503a704ef9cb6ed83c58625f87
Laboratorio <laboratorio@udla-ec.int>
2026-05-05 12:04:51 -0500

    Tercer commit: agregamos punteros con float en mi programa

--- punteros_v1.c
+++ punteros_v1.c
@@ -14,6 +14,12 @@ int main(int argc, char const *argv[])
     c = *ptr; 
     printf ("%p %c\n", ptr, c);
 
+    float decimal = 3.14, dec; 
+    float *ptrF; 
+    ptrF = &decimal; 
+    dec = *ptrF; 
+    printf ("%p %.2f\n", ptrF, dec); 
+
 
     return 0;
 }

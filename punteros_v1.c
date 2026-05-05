a53c4eb796085284e4df1c917506d4932d64a9fc
Laboratorio <laboratorio@udla-ec.int>
2026-05-05 11:52:25 -0500

    Primer commit: puntero con entero

---
+++ punteros_v1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num=15, a;
+    int *x;
+
+    x = &num;  //a x se asigna la direccion de num
+    a = *x;    //a se le asigna el valor de x
+    printf("%p %d\n",x,a);
+
+
+    return 0;
+}
